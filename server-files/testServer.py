df51a85cf7d68e092b4d685ec9293321dcdf2217
Runtness <tarandorland@protonmail.com>
2019-09-10 12:05:10 -0400

    Minor context updates, private messaging system completed

--- server-files/testServer.py
+++ server-files/testServer.py
@@ -64,15 +64,16 @@ def broadcastChannel(name, message, channel):
 #Broadcast a message to a specified user from another user (Private message)
 def boradcastPrivateMsg(name, to_name, message):
     msg = Fore.MAGENTA + "{0}@{1}=> {2}".format(name, to_name, message) + Style.RESET_ALL
-    print(msg)
 
+    #Check if the user actually exists
     if to_name in users:
+        print(msg)
         try:
             users[to_name].sendall(msg.encode('utf-8'))
         except socket.error:
             pass
     else:
-        print(Fore.RED + "{0} attempted to send message to {1}: Error user doesn't exist.".format(name, to_name))
+        print(Fore.RED + "{0} attempted to send message to {1}: Error user doesn't exist.".format(name, to_name) + Style.RESET_ALL)
         replyMsg = Fore.RED + "Error: User {0} does not exist.".format(to_name) + Style.RESET_ALL
         try:
             users[name].sendall(replyMsg.encode('utf-8'))
